c4080fcec22f19db3f21ed5be403911b819d9e00
vladimirstanica <vladimirstanica1@gmail.com>
2025-12-09 08:58:43 +0200

    added exception handler

--- src/main/java/com/smartbill/fibonacci/controller/GlobalExceptionHandler.java
+++ src/main/java/com/smartbill/fibonacci/controller/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.smartbill.fibonacci.exception.InvalidJwtException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -45,9 +44,12 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(TokenExpiredException.class)
-    public ResponseEntity<?> handleExpired(TokenExpiredException ex) {
-        return ResponseEntity.status(UNAUTHORIZED)
-                .body("Token expired");
+    public ResponseEntity<Map<String, Object>> handleTokenExpiredException(TokenExpiredException ex) {
+        Map<String, Object> body = Map.of(
+                "timestamp", Instant.now(),
+                "message", ex.getMessage()
+        );
+        return new ResponseEntity<>(body, UNAUTHORIZED);
     }
 
 
